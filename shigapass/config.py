4321cb2c7a346b2f72f580184a7ac7d176a8ab7a
ryanjameskennedy <ryanjameskennedy@icloud.com>
2024-04-17 18:12:14 +0200

    Update config.py

--- shigapass/config.py
+++ shigapass/config.py
@@ -1,17 +1,16 @@
 db_config_array = [
-    {"db_fasta": "IPAH/ipaH_150-mers.fasta", "db_marker": "ipaH", "identity": 98, "coverage": 95},
-    {"db_fasta": "RFB/RFB_serotypes_AtoC_150-mers_v2.fasta", "db_marker": "rfb", "identity": 98, "coverage": 95},
-    {"db_fasta": "RFB/galF_SB1.fasta", "db_marker": "additionalrfb", "identity": 98, "coverage": 95},
-    {"db_fasta": "RFB/RFB_serogroup_D_150-mers.fasta", "db_marker": "additionalrfb", "identity": 100, "coverage": 100},
-    {"db_fasta": "RFB/POAC-genes_150-mers.fasta", "db_marker": "POAC", "identity": 98, "coverage": 95},
-    {"db_fasta": "FLIC/fliC_Shigella_v1.fasta", "db_marker": "flic", "identity": 98, "coverage": 95},
-    {"db_fasta": "CRISPR/CRISPR_spacers.fasta", "db_marker": "crispr", "identity": 100, "coverage": 100}
+    {"db_fasta": "IPAH/ipaH_150-mers.fasta", "db_marker": "ipaH", "identity": 98, "coverage": 95, "rfb": None},
+    {"db_fasta": "RFB/RFB_serotypes_AtoC_150-mers_v2.fasta", "db_marker": "rfb", "identity": 98, "coverage": 95, "rfb": None},
+    {"db_fasta": "RFB/galF_SB1.fasta", "db_marker": "additionalrfb", "identity": 98, "coverage": 95, "rfb": "C1"},
+    {"db_fasta": "RFB/RFB_serogroup_D_150-mers.fasta", "db_marker": "additionalrfb", "identity": 100, "coverage": 100, "rfb": None},
+    {"db_fasta": "RFB/POAC-genes_150-mers.fasta", "db_marker": "POAC", "identity": 98, "coverage": 95, "rfb": None},
+    {"db_fasta": "FLIC/fliC_Shigella_v1.fasta", "db_marker": "flic", "identity": 98, "coverage": 95, "rfb": None},
+    {"db_fasta": "CRISPR/CRISPR_spacers.fasta", "db_marker": "crispr", "identity": 100, "coverage": 100, "rfb": None}
 ]
 
-db_alt_config_array = {
-    "additionalrfb": {
-        "rfbs": ["A2", "A3a", "A3b", "C10"],
-        "files": ["RFB_AprovBEDP02-5104_150-mers.fasta", "RFB_A16_150-mers_v2.fasta", "RFB_A16_150-mers_v2.fasta", "taurine_SB6.fasta"],
-        "new_rfbs": ["AprovBEDP02-5104", "A16", "A16", "C6"],
-    }
-}
+db_alt_config_array = [
+    {"db_fasta": "RFB/RFB_AprovBEDP02-5104_150-mers.fasta", "db_marker": "additionalrfb", "identity": 98, "coverage": 95, "rfb": "A2", "new_rfb": "AprovBEDP02-5104"},
+    {"db_fasta": "RFB/RFB_A16_150-mers_v2.fasta", "db_marker": "additionalrfb", "identity": 98, "coverage": 95, "rfb": "A3a", "new_rfb": "A16"},
+    {"db_fasta": "RFB/RFB_A16_150-mers_v2.fasta", "db_marker": "additionalrfb", "identity": 98, "coverage": 95, "rfb": "A3b", "new_rfb": "A16"},
+    {"db_fasta": "RFB/taurine_SB6.fasta", "db_marker": "additionalrfb", "identity": 98, "coverage": 95, "rfb": "C10", "new_rfb": "C6"},
+]
